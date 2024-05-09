ce28c30e0622ad4e58f6a2ae4928fa341741d0bb
ShubhamChidrewar <shubhamhidrewar11@gmail.com>
2024-05-09 16:30:23 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Parent = React.createElement(
-  "div",
-  { className: "container" },
-  [
-    React.createElement("h1", { id: "title" }, "Heading"),
-    React.createElement("p", { className: "desc" }, "This is description"),
-  ],
-  React.createElement("div", { className: "container" }, [
-    React.createElement("h1", { id: "title" }, "Heading"),
-    React.createElement("p", { className: "desc" }, "This is description"),
-  ])
-);
+// React Element
+const Heading = () => <h1>This is JSX Heading</h1>;
+
+// React Functional Component
+
+function HeadingComp() {
+  return <h3>This is nomal function component</h3>;
+}
+
+// Component Composition --> One compoent in another one
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Heading />
+      <HeadingComp />
+      {2 + 2}
+      <h1>This is Heading from Functional Component</h1>
+    </div>
+  );
+};
+
+function HeadingComp() {
+  return <h3>This is nomal function component</h3>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Parent);
+
+root.render(<HeadingComponent />);
